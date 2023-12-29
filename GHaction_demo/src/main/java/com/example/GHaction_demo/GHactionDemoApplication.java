7867f71db3bd4bd57a78ce06efd9a5f29579bc5e
nivedhanr <nivedharenahan@gmail.com>
2023-12-29 14:38:10 +0530

    first commit

---
+++ GHaction_demo/src/main/java/com/example/GHaction_demo/GHactionDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.GHaction_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GHactionDemoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		SpringApplication.run(GHactionDemoApplication.class, args);
+	}
+
+}
---
+++ GHaction_demo/src/test/java/com/example/GHaction_demo/GHactionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GHaction_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GHactionDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
